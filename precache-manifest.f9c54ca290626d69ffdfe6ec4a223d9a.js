702e1e252adb3d39baf7c1f41c1c43d89b2010d1
streloc84 <streloc84@gmail.com>
2020-02-02 16:28:58 +0400

    Updates

--- precache-manifest.f9c54ca290626d69ffdfe6ec4a223d9a.js
+++ precache-manifest.f9c54ca290626d69ffdfe6ec4a223d9a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "82de8edb74f19a8a19a2aeb5c58d5d5f",
+    "revision": "93cf9d87348469077f1c808d66c9110c",
     "url": "/streloc84.github.io/index.html"
   },
   {
-    "revision": "b9278627e87d3480b4fd",
-    "url": "/streloc84.github.io/static/css/main.ad24db5d.chunk.css"
+    "revision": "0daa74d98e384b61c55b",
+    "url": "/streloc84.github.io/static/css/main.52439501.chunk.css"
   },
   {
-    "revision": "e71ba9dbd4a5c9d75bbe",
-    "url": "/streloc84.github.io/static/js/2.92e4bd99.chunk.js"
+    "revision": "5fab4821a3863ae94d04",
+    "url": "/streloc84.github.io/static/js/2.9aff1778.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "/streloc84.github.io/static/js/2.92e4bd99.chunk.js.LICENSE"
+    "url": "/streloc84.github.io/static/js/2.9aff1778.chunk.js.LICENSE"
   },
   {
-    "revision": "b9278627e87d3480b4fd",
-    "url": "/streloc84.github.io/static/js/main.7c0a7a72.chunk.js"
+    "revision": "0daa74d98e384b61c55b",
+    "url": "/streloc84.github.io/static/js/main.9ec6668c.chunk.js"
   },
   {
     "revision": "ec46f61669cf277dd387",
@@ -28,8 +28,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/streloc84.github.io/static/media/b52_[allfont.ru].f38166e0.ttf"
   },
   {
-    "revision": "05d39315961d90930e1ab50e5f964d35",
-    "url": "/streloc84.github.io/static/media/paper_clip.05d39315.svg"
+    "revision": "37ae3287f1a6936c946700be07ff67a3",
+    "url": "/streloc84.github.io/static/media/paper_clip.37ae3287.svg"
   },
   {
     "revision": "3c0f6fd6628902b2a8f2a3021b8862d5",

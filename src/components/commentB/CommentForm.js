be82cb9dca0e3f03761ba23c14f041bd38c82153
streloc84 <streloc84@gmail.com>
2020-02-13 20:32:39 +0400

    end

--- src/components/commentB/CommentForm.js
+++ src/components/commentB/CommentForm.js
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
 
 class CommentForm extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            author:"",
+            text:""
+        }
+      }
+   /*  getInitialState(){
+        return {
+            author:"",
+            text:""
+    }
+        } */
+    handleAuthorChange=(e)=>{
+        this.setState({author:e.target.value});
+    }
+    handleTextChange=(e)=>{
+        this.setState({text:e.target.value});
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        var author=this.state.author.trim();
+        var text=this.state.text.trim();
+        if(!text||!author){
+            return;
+        }
+        this.props.onCommentSubmit({author:author,text:text});
+        this.setState({author:"",text:""});
+    }
     render(){
         return(
-            <div className="commentForm">
-                Hello world! I am a commentForm.
-            </div>
+            <form className="commentForm">
+                <input type="text" placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange}/>
+                <input type="text" placeholder="Say something..." value={this.state.text} onChange={this.handleTextChange}/>
+                <input type="submit" value="Post"/>
+            </form>
         )
     }
 }

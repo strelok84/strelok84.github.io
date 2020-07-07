cb74a6c5cd6c1020489853b839328e866af0018a
streloc84 <streloc84@mail.ru>
2020-07-07 23:06:11 +0400

    weather

--- src/components/CommentBlock.js
+++ src/components/CommentBlock.js
@@ -4,7 +4,13 @@ import Comment from "./Comment";
 function CommentBlock(){
     return(
         <section className="about">
-            <p>Тратата</p>
+            <p>Сделал небольшое погодное приложение, опять таки на React<br />
+               Определяет местоположение пользователя на основе данных navigator.geolocation (или вручную). Данные тянет по API с metaweather.com<br />
+               <a href="https://streloc84.github.io/weather/">https://streloc84.github.io/weather/</a><br />
+               
+
+            </p>
+            <div></div>
             <input type="checkbox" id="hd-3" className="hide"/>
             <label htmlFor="hd-3" >Комментарии</label>
 		    <Comment num="2" />

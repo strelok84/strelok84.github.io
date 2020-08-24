bdbc8623abc7abee6a4b7f87342d0ee0d2e42316
streloc84 <streloc84@gmail.com>
2020-08-24 20:01:00 +0400

    Updates

---
+++ precache-manifest.da91ff954bacec9cba23d47078a25cc5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d03795e7be64a800bd0e5bd31e6ee15",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81c08227ebb2ad589f48",
+    "url": "./static/css/main.1c4565cb.chunk.css"
+  },
+  {
+    "revision": "50560a8d7f470d20a091",
+    "url": "./static/js/2.0f6ff89e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f6ff89e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81c08227ebb2ad589f48",
+    "url": "./static/js/main.89e02187.chunk.js"
+  },
+  {
+    "revision": "26f32038f8251e8978f0",
+    "url": "./static/js/runtime-main.8cc0071d.js"
+  },
+  {
+    "revision": "f38166e0bcbb7663f0d2834ffd52addf",
+    "url": "./static/media/b52_[allfont.ru].f38166e0.ttf"
+  },
+  {
+    "revision": "05d39315961d90930e1ab50e5f964d35",
+    "url": "./static/media/paper_clip.05d39315.svg"
+  },
+  {
+    "revision": "3c0f6fd6628902b2a8f2a3021b8862d5",
+    "url": "./static/media/textured_paper.3c0f6fd6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0f67f126247b406f3e7845562413d558.js"
+  "./precache-manifest.da91ff954bacec9cba23d47078a25cc5.js"
 );
 
 self.addEventListener('message', (event) => {

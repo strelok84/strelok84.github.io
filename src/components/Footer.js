95267f5688737b02b1c02ef08d35b859a5347720
Andrey Bespalov <streloc84@gmail.com>
2020-11-10 17:10:51 +0400

    переезд на k

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="container__footer">
-      <a href="https://github.com/streloc84">
+      <a href="https://github.com/strelok84">
         <img src="img/iconmonstr-github-3.svg" alt="github link"></img>
       </a>
       <a href="https://www.linkedin.com/in/%D0%B0%D0%BD%D0%B4%D1%80%D0%B5%D0%B9-%D0%B1%D0%B5%D1%81%D0%BF%D0%B0%D0%BB%D0%BE%D0%B2-44270217a/">
